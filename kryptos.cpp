dbd079391dd51911dd381a2063819c614c426e0e
Marc Fascia <mfascia@gmail.com>
2024-04-17 16:48:45 +0100

    more k4 experiments

--- kryptos.cpp
+++ kryptos.cpp
@@ -4,9 +4,7 @@
 #include <algorithm>
 #include <fstream>
 
-
 using namespace std;
-
 
 string K1CT = "EMUFPHZLRFAXYUSDJKZLDKRNSHGNFIVJYQTQUXQBQVYUVLLTREVJYQTMKYRDMFD";
 string K1PT = "BETWEENSUBTLESHADINGANDTHEABSENCEOFLIGHTLIESTHENUANCEOFIQLUSION";
@@ -21,30 +19,28 @@ string K4CT = "OBKRUOXOGHULBSOLIFBBWFLRVQQPRNGKSSOTWTQSJQSSEKZZWATJKLUDIAWINFBNY
 string K4CR = "?????????????????????EASTNORTHEAST?????????????????????????????BERLINCLOCK???????????????????????";
 string K4CL = "EASTNORTHEASTBERLINCLOCK";
 
-
 string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-double ENG_FREQS[26] = {    0.081238378, 0.014892788, 0.027114200, 0.043191829, 0.120195499, 
-                            0.023038568, 0.020257483, 0.059214604, 0.073054201, 0.001031250,
-                            0.006895114, 0.039785412, 0.026115862, 0.069477738, 0.076811682,
-                            0.018189498, 0.001124502, 0.060212942, 0.062807524, 0.090985886,
-                            0.028776268, 0.011074969, 0.020948640, 0.001727893, 0.021135143,
-                            0.000702128 };
-
+double ENG_FREQS[26] = {0.081238378, 0.014892788, 0.027114200, 0.043191829, 0.120195499,
+                        0.023038568, 0.020257483, 0.059214604, 0.073054201, 0.001031250,
+                        0.006895114, 0.039785412, 0.026115862, 0.069477738, 0.076811682,
+                        0.018189498, 0.001124502, 0.060212942, 0.062807524, 0.090985886,
+                        0.028776268, 0.011074969, 0.020948640, 0.001727893, 0.021135143,
+                        0.000702128};
 
 string GenAlphabet(string _Key)
 {
     string generated;
 
-    for(int i=0; i <_Key.length(); ++i)
+    for (int i = 0; i < _Key.length(); ++i)
     {
-        if(generated.find(_Key[i]) == string::npos)
+        if (generated.find(_Key[i]) == string::npos)
         {
             generated += _Key[i];
         }
     }
-    for(int i=0; i <ALPHABET.length(); ++i)
+    for (int i = 0; i < ALPHABET.length(); ++i)
     {
-        if(generated.find(ALPHABET[i]) == string::npos)
+        if (generated.find(ALPHABET[i]) == string::npos)
         {
             generated += ALPHABET[i];
         }
@@ -53,25 +49,24 @@ string GenAlphabet(string _Key)
     return generated;
 }
 
-
-string EncryptVigenere( const string & _Text, const string & _Alphabet, const string & _Key)
+string EncryptVigenere(const string &_Text, const string &_Alphabet, const string &_Key)
 {
     string ct;
     size_t keyLength = _Key.length();
     size_t skipped = 0;
     vector<int> offsets;
 
-    for(size_t i=0; i<keyLength; ++i)
+    for (size_t i = 0; i < keyLength; ++i)
     {
         offsets.push_back(_Alphabet.find(_Key[i]));
     }
 
-    for(size_t i=0; i<_Text.length(); ++i)
+    for (size_t i = 0; i < _Text.length(); ++i)
     {
         char c = _Text[i];
 
         size_t p = _Alphabet.find(c);
-        if(p == string::npos)
+        if (p == string::npos)
         {
             ct += c;
             ++skipped;
@@ -79,7 +74,7 @@ string EncryptVigenere( const string & _Text, const string & _Alphabet, const st
         }
         else
         {
-            p = (p + 26 + offsets[(i-skipped) % keyLength]) % 26;
+            p = (p + 26 + offsets[(i - skipped) % keyLength]) % 26;
             ct += _Alphabet[p];
         }
     }
@@ -87,25 +82,24 @@ string EncryptVigenere( const string & _Text, const string & _Alphabet, const st
     return ct;
 }
 
-
-string DecryptVigenere( const string & _Text, const string & _Alphabet, const string & _Key)
+string DecryptVigenere(const string &_Text, const string &_Alphabet, const string &_Key)
 {
     string pt;
     size_t keyLength = _Key.length();
     size_t skipped = 0;
     vector<int> offsets;
 
-    for(size_t i=0; i<keyLength; ++i)
+    for (size_t i = 0; i < keyLength; ++i)
     {
         offsets.push_back(_Alphabet.find(_Key[i]));
     }
 
-    for(size_t i=0; i<_Text.length(); ++i)
+    for (size_t i = 0; i < _Text.length(); ++i)
     {
         char c = _Text[i];
 
         size_t p = _Alphabet.find(c);
-        if(p == string::npos)
+        if (p == string::npos)
         {
             pt += c;
             ++skipped;
@@ -113,7 +107,7 @@ string DecryptVigenere( const string & _Text, const string & _Alphabet, const st
         }
         else
         {
-            p = (p + 26 - offsets[(i-skipped)% keyLength]) % 26;
+            p = (p + 26 - offsets[(i - skipped) % keyLength]) % 26;
             pt += _Alphabet[p];
         }
     }
@@ -121,23 +115,22 @@ string DecryptVigenere( const string & _Text, const string & _Alphabet, const st
     return pt;
 }
 
-
-vector<double> Histogram(const string & _Text)
+vector<double> Histogram(const string &_Text)
 {
     vector<double> histogram;
     histogram.resize(26);
     size_t count = 0;
 
-    for(int i=0; i<_Text.length(); ++i)
+    for (int i = 0; i < _Text.length(); ++i)
     {
-        if(ALPHABET.find(_Text[i]) != string::npos)
+        if (ALPHABET.find(_Text[i]) != string::npos)
         {
             histogram[_Text[i] - 'A'] += 1.0;
             ++count;
         }
     }
 
-    for(int i=0; i<26; ++i)
+    for (int i = 0; i < 26; ++i)
     {
         histogram[i] = histogram[i] / count;
     }
@@ -145,13 +138,12 @@ vector<double> Histogram(const string & _Text)
     return histogram;
 }
 
-
-double VarianceFromEnglish(const string & _Text)
+double VarianceFromEnglish(const string &_Text)
 {
     vector<double> histogram = Histogram(_Text);
     double variance = 0.0;
 
-    for(int i=0; i<26; ++i)
+    for (int i = 0; i < 26; ++i)
     {
         double d = histogram[i] - ENG_FREQS[i];
         variance += d * d;
@@ -160,12 +152,11 @@ double VarianceFromEnglish(const string & _Text)
     return variance;
 }
 
-
-string RotateCW(const string & _Text, int _NbCols)
+string RotateCW(const string &_Text, int _NbCols)
 {
     string padded = _Text;
 
-    while(padded.length() % _NbCols != 0)
+    while (padded.length() % _NbCols != 0)
     {
         padded.push_back('?');
     }
@@ -175,23 +166,22 @@ string RotateCW(const string & _Text, int _NbCols)
 
     string transposed;
 
-    for(int x=0; x<W; ++x)
+    for (int x = 0; x < W; ++x)
     {
-        for(int y=H-1; y>-1; --y)
+        for (int y = H - 1; y > -1; --y)
         {
-            transposed += padded[y*W + x];
+            transposed += padded[y * W + x];
         }
     }
-    
+
     return transposed;
 }
 
-
-string RotateCCW(const string & _Text, int _NbCols)
+string RotateCCW(const string &_Text, int _NbCols)
 {
     string padded = _Text;
 
-    while(padded.length() % _NbCols != 0)
+    while (padded.length() % _NbCols != 0)
     {
         padded.push_back('?');
     }
@@ -201,37 +191,36 @@ string RotateCCW(const string & _Text, int _NbCols)
 
     string transposed;
 
-    for(int x=W-1; x>-1; --x)
+    for (int x = W - 1; x > -1; --x)
     {
-        for(int y=0; y<H; ++y)
+        for (int y = 0; y < H; ++y)
         {
-            transposed += padded[y*W + x];
+            transposed += padded[y * W + x];
         }
     }
-    
+
     return transposed;
 }
 
-
-bool HasCribLetters(const string & _Text, const string & _Crib)
+bool HasCribLetters(const string &_Text, const string &_Crib)
 {
     vector<size_t> positions;
     size_t textLength = _Text.length();
 
-    for(int i=0; i<_Crib.length(); ++i)
+    for (int i = 0; i < _Crib.length(); ++i)
     {
         char c = _Crib[i];
         bool found = false;
-        for(int j=0; j<textLength; ++j)
+        for (int j = 0; j < textLength; ++j)
         {
-            if(_Text[j] == c && find(positions.begin(), positions.end(), j) == positions.end())
+            if (_Text[j] == c && find(positions.begin(), positions.end(), j) == positions.end())
             {
                 positions.push_back(j);
                 found = true;
                 break;
-            }            
+            }
         }
-        if(!found)
+        if (!found)
         {
             return false;
         }
@@ -240,14 +229,13 @@ bool HasCribLetters(const string & _Text, const string & _Crib)
     return true;
 }
 
-
-string SwizzleENDYAHR(const string & _Text)
+string SwizzleENDYAHR(const string &_Text)
 {
     string left;
     string right;
-    for(int i=0; i<_Text.length(); ++i)
+    for (int i = 0; i < _Text.length(); ++i)
     {
-        if((i%7 == 0) || (i%7 == 1) || (i%7 == 2) || (i%7 == 5))
+        if ((i % 7 == 0) || (i % 7 == 1) || (i % 7 == 2) || (i % 7 == 5))
         {
             left += _Text[i];
         }
@@ -259,23 +247,22 @@ string SwizzleENDYAHR(const string & _Text)
     return left + right;
 }
 
-
-string SwizzleENDYAHR_2(const string & _Text)
+string SwizzleENDYAHR_2(const string &_Text)
 {
     string swizzled;
     string left, right;
-    for(int i=0; i<_Text.length(); ++i)
+    for (int i = 0; i < _Text.length(); ++i)
     {
-        if(i%7 == 0)
+        if (i % 7 == 0)
         {
             swizzled += left;
             swizzled += right;
             left.clear();
             right.clear();
         }
-        if( (i%7 == 0) || (i%7 == 1) || (i%7 == 2) || (i%7 == 5))
+        if ((i % 7 == 0) || (i % 7 == 1) || (i % 7 == 2) || (i % 7 == 5))
         {
-            left  += _Text[i]; 
+            left += _Text[i];
         }
         else
         {
@@ -285,24 +272,45 @@ string SwizzleENDYAHR_2(const string & _Text)
     return swizzled;
 }
 
-
-bool isAlpha(const string & _Text)
+string BlockRotate(const string &_Text)
 {
-    auto it = find_if(_Text.begin(), _Text.end(), [](char const &c) {
-            return !isalpha(c);
-        });
- 
+    string left = "?" + _Text.substr(0, 48);
+    string right = _Text.substr(48);
+
+    return RotateCW(left, 7) + RotateCCW(right, 7);
+}
+
+bool isAlpha(const string &_Text)
+{
+    auto it = find_if(_Text.begin(), _Text.end(), [](char const &c)
+                      { return !isalpha(c); });
+
     return it == _Text.end();
 }
 
-string toUpper(const string & _Text)
+string toUpper(const string &_Text)
 {
     string up;
-    for(int i=0; i<_Text.length(); ++i)
+    for (int i = 0; i < _Text.length(); ++i)
+    {
+
         up += toupper(_Text[i]);
+    }
     return up;
 }
 
+string makeAlpha(const string &_Text)
+{
+    string alpha;
+    for (int i = 0; i < _Text.length(); ++i)
+    {
+        if (isalpha(_Text[i]))
+        {
+            alpha += _Text[i];
+        }
+    }
+    return alpha;
+}
 
 void RunTests()
 {
@@ -326,15 +334,11 @@ void RunTests()
     printf("%i\n", HasCribLetters("ABCDEABCDE", "BCBEX"));
 }
 
-
 int main()
 {
     RunTests();
 
     string kAlphabet = GenAlphabet("KRYPTOS");
-
-    ifstream file("Words.txt");
-    string word; 
 
     struct Match
     {
@@ -343,53 +347,52 @@ int main()
         double variance;
     };
 
-    vector<Match> matches;
-
-    string ct = K4CT;
-    // reverse(ct.begin(), ct.end());
-
-    while (getline(file, word))
+    for (int r = 2; r < 49; ++r)
     {
-        word = toUpper(word);
-        if(!isAlpha(word))
+        string ct = makeAlpha(RotateCW(K4CT, r));
+        reverse(ct.begin(), ct.end());
+
+        printf("%i\t%s\n", r, ct.c_str());
+
+        vector<Match> matches;
+        ifstream file("Words.txt");
+        string word;
+        while (getline(file, word))
         {
-            continue;
+            word = toUpper(word);
+            if (!isAlpha(word))
+            {
+                continue;
+            }
+
+            string pt = DecryptVigenere(ct, kAlphabet, word);
+            if (HasCribLetters(pt, K4CL))
+            {
+                Match m;
+                m.keyword = word;
+                m.pt = pt;
+                m.variance = VarianceFromEnglish(pt);
+                matches.push_back(m);
+            }
         }
 
-        string pt = DecryptVigenere(ct, kAlphabet, word);
-        if(HasCribLetters(pt, K4CL))
-        {
-            Match m;
-            m.keyword = word;
-            m.pt = pt;
-            m.variance = VarianceFromEnglish(pt);
-            matches.push_back(m);
-        }
+        sort(matches.begin(), matches.end(), [](const Match &a, const Match &b)
+             { return a.variance < b.variance; });
+
+        printf("%s\t%f\n", matches[0].keyword.c_str(), matches[0].variance);
+        printf("%s\n", matches[0].pt.c_str());
+        printf("%s\n\n", K4CR.c_str());
+
+        // for (int i = 0; i < matches.size(); ++i)
+        // {
+        //     if (matches[i].pt.find("EAST") != string::npos)
+        //     {
+        //         printf("%s\t%f\n", matches[i].keyword.c_str(), matches[i].variance);
+        //         printf("%s\n", matches[i].pt.c_str());
+        //         printf("%s\n\n", K4CR.c_str());
+        //     }
+        // }
     }
-
-    sort(matches.begin(), matches.end(),[](const Match & a, const Match & b)
-    {
-        return a.variance < b.variance;
-    });
-
-    for(int i=0; i<matches.size(); ++i)
-    {
-        // string r1 = RotateCW(matches[i].pt, 7);
-        // string r2 = RotateCW(r1, 14);
-        // printf("%s\t%f\n", matches[i].keyword.c_str(), matches[i].variance);
-        // printf("%s\n", matches[i].pt.c_str());
-        // printf("%s\n", r1.c_str());
-        // printf("%s\n", r2.c_str());
-        string swizzled = SwizzleENDYAHR_2(matches[i].pt);
-
-        if(swizzled.find("BERLIN") != string::npos)
-        {
-            printf("%s\n", swizzled.c_str());
-            printf("%s\n\n", K4CR.c_str());
-        }
-}
-
-
 
     return 0;
 }
